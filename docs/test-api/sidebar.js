e60ef70a507899a728ac8f73e4518ac6e5016fa6
Eric Lin <eric.lin@consensys.net>
2023-01-23 10:23:52 +1000

    fix: tab indentation code

--- docs/test-api/sidebar.js
+++ docs/test-api/sidebar.js
@@ -1,1 +1,156 @@
-module.exports = [{"type":"doc","id":"test-api/swagger-petstore-yaml"},{"type":"category","label":"Pets","link":{"type":"doc","id":"test-api/pet"},"items":[{"type":"doc","id":"test-api/add-pet","label":"Add a new pet to the store","className":"api-method post"},{"type":"doc","id":"test-api/update-pet","label":"Update an existing pet","className":"api-method put"},{"type":"doc","id":"test-api/get-pet-by-id","label":"Find pet by ID","className":"api-method get"},{"type":"doc","id":"test-api/update-pet-with-form","label":"Updates a pet in the store with form data","className":"api-method post"},{"type":"doc","id":"test-api/delete-pet","label":"Deletes a pet","className":"api-method delete"},{"type":"doc","id":"test-api/upload-file","label":"uploads an image","className":"api-method post"},{"type":"doc","id":"test-api/find-pets-by-status","label":"Finds Pets by status","className":"api-method get"},{"type":"doc","id":"test-api/find-pets-by-tags","label":"Finds Pets by tags","className":"menu__list-item--deprecated api-method get"},{"type":"doc","id":"test-api/new-pet","label":"New pet","className":"api-method event"}]},{"type":"category","label":"Petstore Orders","link":{"type":"doc","id":"test-api/store"},"items":[{"type":"doc","id":"test-api/get-inventory","label":"Returns pet inventories by status","className":"api-method get"},{"type":"doc","id":"test-api/place-order","label":"Place an order for a pet","className":"api-method post"},{"type":"doc","id":"test-api/get-order-by-id","label":"Find purchase order by ID","className":"api-method get"},{"type":"doc","id":"test-api/delete-order","label":"Delete purchase order by ID","className":"api-method delete"},{"type":"doc","id":"test-api/subscribe-to-the-store-events","label":"Subscribe to the Store events","className":"api-method post"}]},{"type":"category","label":"Users","link":{"type":"doc","id":"test-api/user"},"items":[{"type":"doc","id":"test-api/create-user","label":"Create user","className":"api-method post"},{"type":"doc","id":"test-api/get-user-by-name","label":"Get user by user name","className":"api-method get"},{"type":"doc","id":"test-api/update-user","label":"Updated user","className":"api-method put"},{"type":"doc","id":"test-api/delete-user","label":"Delete user","className":"api-method delete"},{"type":"doc","id":"test-api/create-users-with-array-input","label":"Creates list of users with given input array","className":"api-method post"},{"type":"doc","id":"test-api/create-users-with-list-input","label":"Creates list of users with given input list","className":"api-method post"},{"type":"doc","id":"test-api/login-user","label":"Logs user into the system","className":"api-method get"},{"type":"doc","id":"test-api/logout-user","label":"Logs out current logged in user session","className":"api-method get"}]}];
+module.exports = [
+  { type: "doc", id: "test-api/swagger-petstore-yaml" },
+  {
+    type: "category",
+    label: "Pets",
+    link: { type: "doc", id: "test-api/pet" },
+    items: [
+      {
+        type: "doc",
+        id: "test-api/add-pet",
+        label: "Add a new pet to the store",
+        className: "api-method post",
+      },
+      {
+        type: "doc",
+        id: "test-api/update-pet",
+        label: "Update an existing pet",
+        className: "api-method put",
+      },
+      {
+        type: "doc",
+        id: "test-api/get-pet-by-id",
+        label: "Find pet by ID",
+        className: "api-method get",
+      },
+      {
+        type: "doc",
+        id: "test-api/update-pet-with-form",
+        label: "Updates a pet in the store with form data",
+        className: "api-method post",
+      },
+      {
+        type: "doc",
+        id: "test-api/delete-pet",
+        label: "Deletes a pet",
+        className: "api-method delete",
+      },
+      {
+        type: "doc",
+        id: "test-api/upload-file",
+        label: "uploads an image",
+        className: "api-method post",
+      },
+      {
+        type: "doc",
+        id: "test-api/find-pets-by-status",
+        label: "Finds Pets by status",
+        className: "api-method get",
+      },
+      {
+        type: "doc",
+        id: "test-api/find-pets-by-tags",
+        label: "Finds Pets by tags",
+        className: "menu__list-item--deprecated api-method get",
+      },
+      {
+        type: "doc",
+        id: "test-api/new-pet",
+        label: "New pet",
+        className: "api-method event",
+      },
+    ],
+  },
+  {
+    type: "category",
+    label: "Petstore Orders",
+    link: { type: "doc", id: "test-api/store" },
+    items: [
+      {
+        type: "doc",
+        id: "test-api/get-inventory",
+        label: "Returns pet inventories by status",
+        className: "api-method get",
+      },
+      {
+        type: "doc",
+        id: "test-api/place-order",
+        label: "Place an order for a pet",
+        className: "api-method post",
+      },
+      {
+        type: "doc",
+        id: "test-api/get-order-by-id",
+        label: "Find purchase order by ID",
+        className: "api-method get",
+      },
+      {
+        type: "doc",
+        id: "test-api/delete-order",
+        label: "Delete purchase order by ID",
+        className: "api-method delete",
+      },
+      {
+        type: "doc",
+        id: "test-api/subscribe-to-the-store-events",
+        label: "Subscribe to the Store events",
+        className: "api-method post",
+      },
+    ],
+  },
+  {
+    type: "category",
+    label: "Users",
+    link: { type: "doc", id: "test-api/user" },
+    items: [
+      {
+        type: "doc",
+        id: "test-api/create-user",
+        label: "Create user",
+        className: "api-method post",
+      },
+      {
+        type: "doc",
+        id: "test-api/get-user-by-name",
+        label: "Get user by user name",
+        className: "api-method get",
+      },
+      {
+        type: "doc",
+        id: "test-api/update-user",
+        label: "Updated user",
+        className: "api-method put",
+      },
+      {
+        type: "doc",
+        id: "test-api/delete-user",
+        label: "Delete user",
+        className: "api-method delete",
+      },
+      {
+        type: "doc",
+        id: "test-api/create-users-with-array-input",
+        label: "Creates list of users with given input array",
+        className: "api-method post",
+      },
+      {
+        type: "doc",
+        id: "test-api/create-users-with-list-input",
+        label: "Creates list of users with given input list",
+        className: "api-method post",
+      },
+      {
+        type: "doc",
+        id: "test-api/login-user",
+        label: "Logs user into the system",
+        className: "api-method get",
+      },
+      {
+        type: "doc",
+        id: "test-api/logout-user",
+        label: "Logs out current logged in user session",
+        className: "api-method get",
+      },
+    ],
+  },
+];
