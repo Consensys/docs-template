69fb6f8e2b630f8d4ff169ea14ffac7e125b1e88
Eric Lin <eric.lin@consensys.net>
2023-01-30 10:52:50 +1000

    feat: add quick links instead of default

---
+++ src/components/HomepageCards/index.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+// import styles from "./styles.module.css";
+
+type CardItem = {
+  title: string;
+  link: string;
+  description: JSX.Element;
+  buttonName: string;
+  buttonType:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "info"
+    | "warning"
+    | "danger"
+    | "link";
+};
+
+const CardList: CardItem[] = [
+  {
+    title: "🏁 Getting Started",
+    link: "/docs/getting-started",
+    description: (
+      <>
+        Come check out the fastest way to get started with this template repo!
+      </>
+    ),
+    buttonName: "Create new docs!",
+    buttonType: "success",
+  },
+  {
+    title: "💭 Repo Layout",
+    link: "/docs/getting-started/understanding-repo-layout",
+    description: (
+      <>
+        See how this repo is laid out to understand what each file and folder is
+        responsible for.
+      </>
+    ),
+    buttonName: "Go to repo layout",
+    buttonType: "secondary",
+  },
+  {
+    title: "👨‍💻 Documentation style guide",
+    link: "/docs/getting-started/style-guide",
+    description: (
+      <>
+        Find our standard documentation style guide to follow when applying to
+        your docs repo.
+      </>
+    ),
+    buttonName: "Go to reference",
+    buttonType: "info",
+  },
+];
+
+function Card({ title, link, description, buttonName, buttonType }: CardItem) {
+  return (
+    <div className={clsx("col", "col--4", "margin-top--md")}>
+      <div className="card-demo">
+        <div className="card">
+          <div className="card__header">
+            <h3>{title}</h3>
+          </div>
+          <div className="card__body">
+            <p>{description}</p>
+          </div>
+          <div className="card__footer">
+            <Link
+              className={clsx(
+                "button",
+                "button--" + buttonType,
+                "button--block",
+              )}
+              to={link}>
+              {buttonName}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageCards(): JSX.Element {
+  return (
+    <section className={clsx("margin-top--lg", "margin-bottom--lg")}>
+      <div className="container">
+        <h1>Quick Links</h1>
+        <hr />
+        <div className="row">
+          {CardList.map((props, idx) => (
+            <Card key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import HomepageCards from "@site/src/components/HomepageCards";
 
 import styles from "./index.module.css";
 
@@ -34,7 +34,8 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <HomepageCards />
+        {/* <HomepageFeatures /> */}
       </main>
     </Layout>
   );
