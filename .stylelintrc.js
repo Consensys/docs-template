0bd0d5e9578a91f3d4d51f5b21ef6f8cb461b4c7
Eric Lin <eric.lin@consensys.net>
2023-02-14 17:08:43 +1000

    chore: update package vers

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  extends: ["stylelint-config-standard"],
   rules: {
     "selector-pseudo-class-no-unknown": [
       true,
